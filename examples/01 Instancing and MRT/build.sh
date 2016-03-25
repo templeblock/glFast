b15a7a2c091d5446b9e63009fa78cb7eaf18ade4
Constantine Tarasenkov <constantine@protonmail.com>
2016-03-25 22:53:16 +0300

    Use default compiler in build scripts

--- examples/01 Instancing and MRT/build.sh
+++ examples/01 Instancing and MRT/build.sh
@@ -3,11 +3,10 @@ cd "$(dirname -- "$(readlink -fn -- "${0}")")"
 
 app="bin/app"
 
-cflags="-std=c11 -g -O0 -Werror=return-type -Wconversion -Wgnu-empty-initializer"
+cflags="-std=c11 -g -O0 -Werror=return-type -Wconversion"
 lflags="-lSDL2 -lm ${@}"
 
 set -x
 
 rm -f $app
-
-clang -o $app main.c $cflags $lflags
+cc -o $app main.c $cflags $lflags
